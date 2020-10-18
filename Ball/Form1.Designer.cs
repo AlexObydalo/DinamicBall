1471b7d95727d70fce6bcd6da7e79175738d0db6
Alex <ochag@ua.fm>
2020-10-18 10:30:13 +0300

    Add coment

---
+++ Ball/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelG = new System.Windows.Forms.Label();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelM = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelp = new System.Windows.Forms.Label();
+            this.Spring = new System.Windows.Forms.PictureBox();
+            this.Ball2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spring)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Бахнуть мяч";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Chartreuse;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 508);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(841, 183);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Ball
+            // 
+            this.Ball.Image = ((System.Drawing.Image)(resources.GetObject("Ball.Image")));
+            this.Ball.Location = new System.Drawing.Point(30, 471);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(28, 31);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            this.Ball.Click += new System.EventHandler(this.Ball_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(688, 81);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = -1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(688, 132);
+            this.trackBar2.Maximum = 1000;
+            this.trackBar2.Minimum = -1000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(670, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(670, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "y";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(700, 113);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(0, 13);
+            this.labelX.TabIndex = 7;
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(700, 164);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(0, 13);
+            this.labelY.TabIndex = 8;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(688, 180);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 9;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(616, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Гравитация";
+            // 
+            // labelG
+            // 
+            this.labelG.AutoSize = true;
+            this.labelG.Location = new System.Drawing.Point(700, 212);
+            this.labelG.Name = "labelG";
+            this.labelG.Size = new System.Drawing.Size(0, 13);
+            this.labelG.TabIndex = 11;
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(688, 225);
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 45);
+            this.trackBar4.TabIndex = 12;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(642, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Масса";
+            // 
+            // labelM
+            // 
+            this.labelM.AutoSize = true;
+            this.labelM.Location = new System.Drawing.Point(700, 260);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(0, 13);
+            this.labelM.TabIndex = 14;
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(688, 276);
+            this.trackBar5.Maximum = 100;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(104, 45);
+            this.trackBar5.TabIndex = 15;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(578, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Плотность воздуха";
+            // 
+            // labelp
+            // 
+            this.labelp.AutoSize = true;
+            this.labelp.Location = new System.Drawing.Point(700, 308);
+            this.labelp.Name = "labelp";
+            this.labelp.Size = new System.Drawing.Size(0, 13);
+            this.labelp.TabIndex = 17;
+            // 
+            // Spring
+            // 
+            this.Spring.Image = ((System.Drawing.Image)(resources.GetObject("Spring.Image")));
+            this.Spring.Location = new System.Drawing.Point(351, 25);
+            this.Spring.Name = "Spring";
+            this.Spring.Size = new System.Drawing.Size(32, 77);
+            this.Spring.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Spring.TabIndex = 21;
+            this.Spring.TabStop = false;
+            // 
+            // Ball2
+            // 
+            this.Ball2.Image = ((System.Drawing.Image)(resources.GetObject("Ball2.Image")));
+            this.Ball2.Location = new System.Drawing.Point(351, 89);
+            this.Ball2.Name = "Ball2";
+            this.Ball2.Size = new System.Drawing.Size(32, 37);
+            this.Ball2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Ball2.TabIndex = 23;
+            this.Ball2.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Firebrick;
+            this.pictureBox2.Location = new System.Drawing.Point(291, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(161, 34);
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 687);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Ball2);
+            this.Controls.Add(this.Spring);
+            this.Controls.Add(this.labelp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.labelM);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.labelG);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spring)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelG;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelM;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelp;
+        private System.Windows.Forms.PictureBox Spring;
+        private System.Windows.Forms.PictureBox Ball2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Ball/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ball
+{
+    public partial class Form1 : Form
+    {
+        Vector v, a, r, g;
+        Vector r0, r2, v2, a2;
+        double t = 0;
+        double m=1, m2=1;
+        double p=1, k;
+        Point start;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            labelX.Text = Convert.ToString(trackBar1.Value);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            labelY.Text = Convert.ToString(trackBar2.Value);
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            labelG.Text = "g="+Convert.ToString(trackBar3.Value)+"м/с^2";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            labelM.Text = "m=" + Convert.ToString(trackBar4.Value) + "кг";
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            labelp.Text = "p=" + Convert.ToString(trackBar5.Value) + "кг/м^3";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            double dt = timer1.Interval / 1000.0;
+            
+            g = new Vector(0, trackBar3.Value*10);
+            Vector F = m * g - v*p/10;
+
+            a = F / m;
+            v += a*dt;
+            r +=  v * dt;
+
+            Vector F2 = 50*(r0-r2);
+
+            a2 = F2 / m2;
+            v2 += a2 * dt;
+            r2 += v2 * dt;
+
+            Ball.Location = new Point ((int)r.X, (int)r.Y);
+            Ball2.Location = new Point((int)r2.X, (int)r2.Y);
+            Spring.Height = Ball2.Top - Spring.Top;
+            if (Ball.Top + Ball.Height > pictureBox1.Top)
+            {
+                
+                v = v.Mirror(new Vector(1, 0)) * 0.8;
+                
+                if(v.SqAbs<0.001)
+                {
+                    timer1.Stop();
+                }
+                
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start = new Point(Ball.Location.X,Ball.Location.Y);
+            r = new Vector(Ball.Left, Ball.Top);
+            r2 = new Vector(Ball2.Left, Ball2.Top);
+            r0 = new Vector(Ball2.Left, Ball2.Top+20);
+            g = new Vector(0, trackBar3.Value*10);
+            t = 0;
+            m = trackBar4.Value;
+            m2 = 1;
+            p = trackBar5.Value;
+            v = new Vector(trackBar1.Value,-trackBar2.Value);
+            v2 = new Vector(0, 0);
+            k = 10;
+            timer1.Start();
+        }
+
+        private void Ball_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
